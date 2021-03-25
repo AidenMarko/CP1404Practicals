4d6c09db040f0fe94cddc6e8629d436faeb0fde5
Aiden <aiden.marko@my.jcu.edu.au>
2021-03-26 06:39:04 +1000

    Add the displaying of subject information

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,12 +8,21 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    display_subject_details(data)
+
+
+def display_subject_details(data):
+    for i in range(0, 3, 1):
+        subject = data[i][0][0]
+        teacher = data[i][0][1]
+        number_of_students = data[i][0][2]
+        print("{} is taught by {:12s} and has {:3} students".format(subject, teacher, number_of_students))
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    list_of_parts = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,8 +31,11 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        list_of_parts.append([parts])
+        print(list_of_parts)
         print("----------")
     input_file.close()
+    return list_of_parts
 
 
 main()
