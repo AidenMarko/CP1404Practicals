2830276ac10dbb9b33dfbb4bd3f583ace4870dd0
Aiden <aiden.marko@my.jcu.edu.au>
2021-04-19 20:32:14 +1000

    Add Guitar class with init and str methods

---
+++ prac_06/guitar.py
@@ -0,0 +1,13 @@
+"""CP1404/CP5632 Practical - guitar."""
+
+
+class Guitar:
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return "{} ({}) : ${}".format(self.name, self.year, self.cost)
+
