903809093ebef5e5f13cfd9a171a68f0c0fcfcab
Aiden <aiden.marko@my.jcu.edu.au>
2021-05-05 13:10:41 +1000

    Add basic button function responses

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -1,12 +1,21 @@
 from kivy.app import App
 from kivy.lang import Builder
-
+from kivy.core.window import Window
 
 class ConvertMilesKm(App):
     def build(self):
+        Window.size = (200, 100)
         self.title = "Convert Miles Km"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def calculate(self):
         print("Calculate")
+
+    def up(self):
+        print("Up")
+
+    def down(self):
+        print("Down")
+
+ConvertMilesKm().run()
