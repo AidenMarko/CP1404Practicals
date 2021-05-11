c6dfd6a0e8a9d27f75b8420d7252f02066ef6d7f
Aiden <aiden.marko@my.jcu.edu.au>
2021-05-11 20:31:08 +1000

    Add menu for choosing a taxi

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -3,30 +3,47 @@ CP1404/CP5632 Practical
 Taxi class
 """
 
-from prac_08.car import Car
 from prac_08.taxi import Taxi
 from prac_08.silver_service_taxi import SilverServiceTaxi
 
 def main():
-    menu = """
-    Q = Quit
-    C = Choose Taxi
-    D = Drive
-    """
+    menu = """Q = Quit \nC = Choose Taxi\nD = Drive"""
+    current_bill = 0
     print(menu)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            choose_taxi()
+            taxi = choose_taxi()
         elif choice == "D":
             drive()
         else:
             print("Invalid Option")
+        print(current_bill)
+        print(menu)
+        choice = input(">>> ").upper()
+    print("Thank You")
 
 def choose_taxi():
-
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    i = 0
+    for taxi in taxis:
+        car = taxi
+        print("{} - ".format(i), str(car))
+        i += 1
+    print("choose taxi")
+    choice = input(">>> ")
+    while choice != "":
+        if choice == "0":
+            return taxis[0]
+        elif choice == "1":
+            return taxis[1]
+        elif choice == "2":
+            return taxis[2]
+        else:
+            print("Invalid Option")
 
 def drive():
+    print("drive")
 
 
 main()
