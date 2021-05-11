2540e9a8d6797fa640e3a51f9bb0f943119de6ae
Aiden <aiden.marko@my.jcu.edu.au>
2021-05-11 17:41:23 +1000

    Add get_fare() to account for flagfall

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -11,6 +11,10 @@ class SilverServiceTaxi(Taxi):
     def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
         self.price_per_km = Taxi.price_per_km * fanciness
+        """self.get_fare = Taxi.current_fare_distance * self.price_per_km + 4.50"""
 
     def __str__(self):
-        return "{} plus ${:.2f}".format(super().__str__(), self.flagfall)
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(), self.flagfall)
+
+    def get_fare(self):
+        return super().get_fare() + 4.50
