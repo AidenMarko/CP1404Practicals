059a6784a88267269b4c3d5a50832bc630ae4c03
Aiden <aiden.marko@my.jcu.edu.au>
2021-05-11 16:19:04 +1000

    Add testing code for class Taxi

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+
+from prac_08.taxi import Taxi
+
+def main():
+    my_car = Taxi("Prius 1", 100)
+    my_car.drive(40)
+    print("Taxi: {}, Fare Distance: {}, Fare Cost: {}".format(my_car.name, my_car.current_fare_distance, my_car.
+                                                              current_fare_distance * my_car.price_per_km))
+    my_car.start_fare()
+    my_car.drive(100)
+    print("Taxi: {}, Fare Distance: {}, Fare Cost: {}".format(my_car.name, my_car.current_fare_distance, my_car.
+                                                              current_fare_distance * my_car.price_per_km))
+
+
+main()
